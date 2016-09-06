088fb9eccdc8881daacec62bdfb8022402177409
maxiaoqiang <463502825@qq.com>
2016-09-06 11:00:03 +0800

    创建进度圆环项目

---
+++ CirleDemo/CirleDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CirleDemo
+//
+//  Created by 深圳市泥巴装网络科技有限公司 on 16/9/6.
+//  Copyright © 2016年 马晓强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
